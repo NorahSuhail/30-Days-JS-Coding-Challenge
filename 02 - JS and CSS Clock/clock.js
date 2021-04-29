2ed8280eb5a4446a730ec7c199363ccae6b8c934
norsuhail <norsuhail@hotmail.com>
2021-04-29 18:22:19 +0300

    Challange 02 compleate

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -6,15 +6,15 @@ function setDate(){
   const now = new Date();
 
   const second = now.getSeconds();
-  const secondDegrees = ((second / 60) * 360) +90;
+  const secondDegrees = ((second / 60) * 360) ;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
   const minute = now.getMinutes();
-  const minuteDegrees = ((minute / 60) * 360) + ((second/60)*6) + 90;
+  const minuteDegrees = ((minute / 60) * 360) + ((second/60)*6) ;
   minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
   const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((minute/60)*30) + 90;
+  const hourDegrees = ((hour / 12) * 360) + ((minute/60)*30) ;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
