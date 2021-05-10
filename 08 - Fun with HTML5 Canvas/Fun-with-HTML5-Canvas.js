6d4618039915f523642ae2e7bea42160faaaf5e0
norsuhail <norsuhail@hotmail.com>
2021-05-10 22:01:20 +0300

    08 - Fun with HTML5 Canvas Challenge completed

---
+++ 08 - Fun with HTML5 Canvas/Fun-with-HTML5-Canvas.js
@@ -0,0 +1,50 @@
+
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADASS";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 100;
+ctx.globalCompositeOperation = 'multiply';
+let isDrwing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e){
+  if(!isDrwing) return;
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+  if(hue >= 360){
+    hue = 0;
+  }
+  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+    direction =! direction;
+  }
+
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+  isDrwing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrwing = false);
+canvas.addEventListener('mouseout', () => isDrwing = true);
