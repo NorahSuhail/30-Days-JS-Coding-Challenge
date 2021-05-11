75dee7947e01bfe4d315a0ac46a01e3d71c84d14
norsuhail <norsuhail@hotmail.com>
2021-05-11 03:06:10 +0300

    10 - Hold Shift and Check Checkboxes Challenge Completed

---
+++ 10 - Hold Shift and Check Checkboxes/Hold-Shift-and-Check-Checkboxes.js
@@ -0,0 +1,23 @@
+const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
+
+console.log(checkboxes);
+
+let lastChecked;
+
+function handelCheck(e){
+    let inBetween = false;
+    if(e.shiftKey && this.checked){
+        checkboxes.forEach( checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('here');
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+}
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handelCheck));
